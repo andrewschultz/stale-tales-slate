e32ced614a79914aa14af73dc7b0319fe6036778
andrewschultz <schultz.andrew@sbcglobal.net>
2015-07-25 12:02:53 -0500

    icl.pl touchup, plus a page of notes

--- icl.pl
+++ icl.pl
@@ -16,6 +16,7 @@
 
 $myProj = "roiling";
 
+#sensible abbreviations
 $proj{"-3"} = "threediopolis";
 $proj{"-d"} = "dirk";
 $proj{"-r"} = "roiling";
@@ -25,6 +26,11 @@ $proj{"-15"} = "directors";
 $proj{"-dir"} = "directors";
 $proj{"-dob"} = "directors";
 
+#which projects are z machine? Default is glulx
+$zmac{"threediopolis"} = 1;
+$zmac{"dirk"} = 1;
+
+$runBeta = 0;
 $debug = 0;
 $release = 1;
 
@@ -37,11 +43,18 @@ while ($count <= $#ARGV)
   for ($a)
   {
   print "$count of $#ARGV: $a\n";
-  /beta/ && do { $beta = 1 - $beta; $count++; next; };
+  /beta/ && do { $runBeta = 1 - $runBeta; $count++; next; };
+  /-f/ && do { $release = $debug = $beta = 0;
+    if ($a =~ /r/) { $release = 1; }
+    if ($a =~ /d/) { $debug = 1; }
+    if ($a =~ /b/) { $beta = 1; }
+	$count++; next;
+  };
   /-nr/ && do { $release = 0; $count++; next; };
   /-yr/ && do { $release = 1; $count++; next; };
   /-nd/ && do { $debug = 0; $count++; next; };
   /-yd/ && do { $debug = 1; $count++; next; };
+  /-x/ && do { $execute = 1; $count++; next; };
   /-a/ && do { for $entry(@allProj) { runProj($entry); } $count++; next; };
   if ($proj{$a}) { $myProj = $proj{$a}; }
   elsif ($proj{"-$a"}) { $myProj = $proj{"-$a"}; }
@@ -53,20 +66,22 @@ while ($count <= $#ARGV)
 
 sub runProj
 {
-# this is bad coding but there's only one exception for now: Threediopolis.
+# this is bad coding but there's only two exceptions for now: Threediopolis and Dirk.
 $ex = "ulx";
 $gz = "gblorb";
-if ($_[0] =~ /threed/) { $ex = "z8"; $gz = "zblorb";}
+$iflag = "G";
+if ($zmac{$_[0]}) { $ex = "z8"; $gz = "zblorb"; $iflag = "v8"; }
 
 $beta = "c:/games/inform/beta.inform";
 $base = "c:/games/inform/$_[0].inform";
 $bdir = "$base/Build";
 
+#die ("$runBeta $debug $release");
 `cd c:/games/inform/$_[0].inform/Build`;
-if ($beta)
+if ($runBeta)
 {
 print("set HOME=c:\\games\\inform\\beta.inform");
-print "Beta build.\n";
+print "****BETA BUILD****\n";
 system("copy c:\\games\\inform\\$_[0].inform\\source\\story.ni c:\\games\\inform\\beta.inform\\source\\story.ni");
 system("copy c:\\games\\inform\\$_[0].inform\\Release.blurb c:\\games\\inform\\beta.inform\\Release.blurb");
 system("copy c:\\games\\inform\\$_[0].inform\\uuid.txt c:\\games\\inform\\beta.inform\\uuid.txt");
@@ -93,13 +108,15 @@ print "5\n";
 modifyBeta();
 system("\"C:/Program Files (x86)/Inform 7/Compilers/ni\" -release -rules \"C:/Program Files (x86)/Inform 7/Inform7/Extensions\" -package \"$beta\" -extension=$ex");
 system("\"C:/Program Files (x86)/Inform 7/Compilers/cblorb\" -windows $beta/Release.blurb $beta/Build/output.$gz");
+if ($execute) { $execute = 0; `$beta/Release.blurb $beta/Build/output.$gz`; }
 }
 if ($debug)
 {
 system("set HOME=c:\\games\\inform\\beta.inform");
 printf "Debug build.\n";
 system("\"C:/Program Files (x86)/Inform 7/Compilers/ni\" -rules \"C:/Program Files (x86)/Inform 7/Inform7/Extensions\" -package \"C:/games/inform/$_[0].inform\" -extension=$ex");
-system("\"C:/Program Files (x86)/Inform 7/Compilers/inform-633\" -kwSDG +include_path=$base,$bdir $bdir/auto.inf $bdir/output.ulx");
+system("\"C:/Program Files (x86)/Inform 7/Compilers/inform-633\" -kwSD$iflag +include_path=$base,$bdir $bdir/auto.inf $bdir/output.$ex");
+if ($execute) { $execute = 0; `$bdir/output.$ex`; }
 }
 if ($release)
 {
@@ -108,11 +125,12 @@ printf "Release build.\n";
 printf "Generating output.$ex.\n";
 system("\"C:/Program Files (x86)/Inform 7/Compilers/ni\" -release -rules \"C:/Program Files (x86)/Inform 7/Inform7/Extensions\" -package \"$base\" -extension=$ex");
 printf "Generating blorb.$ex.\n";
-system("\"C:/Program Files (x86)/Inform 7/Compilers/inform-633\" -kw~S~DG +include_path=$base,$bdir $bdir/auto.inf $bdir/output.ulx");
+system("\"C:/Program Files (x86)/Inform 7/Compilers/inform-633\" -kw~S~D$iflag +include_path=$base,$bdir $bdir/auto.inf $bdir/output.$ex");
 #the below doesn't work as in the Windows compiler, so we have to explicitly set paths
 #system("\"C:/Program Files (x86)/Inform 7/Compilers/cblorb\" -windows Release.blurb Build/output.gblorb");
 printf "Bundling for release.\n";
 system("\"C:/Program Files (x86)/Inform 7/Compilers/cblorb\" -windows $base/Release.blurb $bdir/output.$gz");
+if ($execute) { $execute = 0; `$bdir/output.$gz`; }
 }
 }
 
