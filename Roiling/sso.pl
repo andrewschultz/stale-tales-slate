54273aa2764faaf02d932472d07ad5f053192adf
andrewschultz <schultz.andrew@sbcglobal.net>
2016-12-03 01:15:42 -0600

    sso now sorts a bit more strictly

--- Roiling/sso.pl
+++ Roiling/sso.pl
@@ -8,25 +8,32 @@
 #puts all of the titles in the middle of the file, if there are any, so I can grab those easily
 #alternative is to get ALL CAPS working
 #
-#future features ALL CAPS separate chunk
+#future features ALL CAPS separate chunk but that'd require renumbering
 #
 #also tacks on quotes
+#
 
 use strict;
 use warnings;
+
+#use Perl::Critic;
 
 ####options
 my $compare = 0;
 my $noCopy = 0;
 
-my @listLump = ("", "", "");
+my @listLump = ("");
 my $roil = "c:\\games\\inform\\roiling.inform\\Source";
 
 my $qad = 0;
 my $titlesPrinted = 0;
 my $readingStrings = 1;
 my $curList = 0;
-my $newTitles;
+
+my $newTitles = "";
+my $allCaps = "";
+my $shoutyAds = "";
+my $biopics = "";
 
 open(A, "$roil\\tosort.txt") || die ("Could not find tosort.txt.");
 
@@ -47,8 +54,16 @@ while (my $line = <A>)
     {
 	  if ($curList == 0)
 	  {
-	    if ($line !~ /^\"/) { $line = "\"$line"; $line =~ s/$/\"/; $qad++; } #add quotes
-	    if ($line =~ /\[r\]/) { $newTitles .= $line; $titlesPrinted++; }
+	    if ($line !~ /^\"/)
+		{
+		  $line = "\"$line";
+		  if ($line =~ /\t/) { $line =~ s/\t/\"\t/; } else { $line =~ s/$/\"/; }
+		  $qad++;
+		} #add quotes
+	    if ($line =~ /\[r\].*\t(true|false)/) { $biopics .= $line; }
+	    elsif ($line =~ /\[r\]/) { $newTitles .= $line; }
+	    elsif ($line !~ /[a-z]/) { $allCaps .= $line; }
+		elsif ($line =~ /\t(true|false)/) { $shoutyAds .= $line; }
 		else { $listLump[$curList] .= $line; }
 	  }
 	  else { $listLump[$curList] .= $line; }
@@ -61,21 +76,21 @@ while (my $line = <A>)
 	{
 	  $readingStrings = 1;
 	  $curList++;
-	  if ($curList > 2) { die("Too many line breaks at $.!"); }
+	  push(@listLump, "");
+	  if ($curList > 5) { die("Too many line breaks at $.!"); }
+	  if ($line =~ /\[r\].*\t(true|false)/) { $listLump[$curList] = $biopics; $biopics = ""; }
+	  if ($line !~ /[a-z]/i) { $listLump[$curList] = $allCaps; $allCaps = ""; }
+	  elsif ($line =~ /\[r\]/i) { $listLump[$curList] = $newTitles; $newTitles = ""; }
+	  elsif ($line =~ /\t(true|false)/i) { $listLump[$curList] = $shoutyAds; $shoutyAds = ""; }
 	  $listLump[$curList] .= $line;
 	}
   }
 }
 
-if ($newTitles)
-{
-  if ($listLump[1])
-  {
-    $listLump[2] = $listLump[1];
-	$listLump[1] = "";
-  }
-  $listLump[1] .= $newTitles;
-}
+my $rolling = 1;
+if ($allCaps) { splice(@listLump, $rolling, 0, $allCaps); $rolling++; }
+if ($newTitles) { splice(@listLump, $rolling, 0, $newTitles); $rolling++; }
+if ($shoutyAds) { splice(@listLump, $rolling, 0, $shoutyAds); $rolling++; }
 
 my @finalOut;
 
@@ -88,9 +103,11 @@ close(B);
 if ($qad) { print "$qad quotes added!\n"; }
 if ($titlesPrinted) { print "$titlesPrinted titles printed!\n"; }
 
-`wm tosort.txt tosort2.txt`;
+if ($compare) { `wm $roil\\tosort.txt $roil\\tosort2.txt`; print "Comparing, no copy over."; exit(); }
+if ($noCopy) { print "Did not copy over. Use -f to force or get rid of -d."; exit(); }
 
-die;
+`copy $roil\\tosort2.txt $roil\\tosort.txt`;
+
 
 sub alfPrint
 {
