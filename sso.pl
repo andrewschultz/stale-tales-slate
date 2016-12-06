a14ebe5fbe516adaf8dec755b9c54a3131c2cddc
andrewschultz <schultz.andrew@sbcglobal.net>
2016-12-06 04:52:03 -0600

    Added/organized area strings

--- sso.pl
+++ sso.pl
@@ -1,6 +1,6 @@
 ###########################
 #sso.pl
-#scratch sort, does a trivial sort of the ideas I have written down
+#scratch sort, does a trivial sort of the anagram ideas I have written down for both SA and ARO
 #
 #unanagrammed ideas at the end
 #anagrammed at the start
@@ -32,21 +32,31 @@ my $readingStrings = 1;
 my $curList = 0;
 
 my $newTitles = "";
+##############################
+#ALL CAPS appears in more than one area but is divisible into the PSA's and the Lament Mantel
 my $allCaps = "";
 my $shoutyAds = "";
 my $biopics = "";
 my $movies = "";
-my $fgp = "";
-my $mgp = "";
-my $igp = "";
-my $xgp = "";
+##############################
+#Shuffling by-area
+my $fgp = ""; #####store f's bad areas
+my $mgp = ""; #####camp/condo/nerds (PSA allcaps yelling goes to shouty)
+my $igp = ""; #####store i's bad poems
+my $xgp = ""; #####names at the start
+my $rgp = ""; #####RBB/rioter taunts
+##############################
+#Roiling by-area (store, then random)
 my $pgp = "";
+my $tgp = "";
+my $ugp = "";
 my $vgp = "";
 my $wgp = "";
 my $ygp = "";
-my $ugp = "";
-my $tgp = "";
-my $hgp = "";
+my $sgp = ""; #####stores has the tokers and the cruelest lectures
+my $hgp = ""; #####store h postfix ending: selly yells and nonsense political themes
+my $ggp = ""; #####generic stuff like waiting or no text
+my $ogp = ""; #####roiling general
 
 if (defined($ARGV[0]))
 {
@@ -87,13 +97,17 @@ while ($line = <A>)
 		elsif ($line =~ /\"m$/) { $mgp .= $line; }
 		elsif ($line =~ /\"i$/) { $igp .= $line; }
 		elsif ($line =~ /\"x$/) { $xgp .= $line; }
+		elsif ($line =~ /\"r$/) { $rgp .= $line; } # end shuffling choices
 		elsif ($line =~ /\"p$/) { $pgp .= $line; }
+		elsif ($line =~ /\"t$/) { $tgp .= $line; }
+		elsif ($line =~ /\"u$/) { $ugp .= $line; }
 		elsif ($line =~ /\"v$/) { $vgp .= $line; }
 		elsif ($line =~ /\"w$/) { $wgp .= $line; }
-		elsif ($line =~ /\"y$/) { $ygp .= $line; }
-		elsif ($line =~ /\"u$/) { $ugp .= $line; }
-		elsif ($line =~ /\"t$/) { $tgp .= $line; }
-		elsif ($line =~ /\"h$/) { $hgp .= $line; }
+		elsif ($line =~ /\"y$/) { $ygp .= $line; } # end store areas
+		elsif ($line =~ /\"s$/) { $sgp .= $line; } # stores
+		elsif ($line =~ /\"h$/) { $hgp .= $line; } # h
+		elsif ($line =~ /\"g$/) { $ggp .= $line; } # generic
+		elsif ($line =~ /\"o$/) { $ogp .= $line; } # opening of ARO
 		else { $listLump[$curList] .= $line; }
 	  }
 	  else { $listLump[$curList] .= $line; }
@@ -117,13 +131,17 @@ while ($line = <A>)
 	  elsif ($line =~ /\"m/i) { $listLump[$curList] = $mgp; $mgp = ""; }
 	  elsif ($line =~ /\"i/i) { $listLump[$curList] = $igp; $igp = ""; }
 	  elsif ($line =~ /\"x/i) { $listLump[$curList] = $xgp; $xgp = ""; }
+	  elsif ($line =~ /\"r/i) { $listLump[$curList] = $rgp; $rgp = ""; }
 	  elsif ($line =~ /\"p/i) { $listLump[$curList] = $pgp; $pgp = ""; }
+	  elsif ($line =~ /\"t/i) { $listLump[$curList] = $tgp; $tgp = ""; }
+	  elsif ($line =~ /\"u/i) { $listLump[$curList] = $ugp; $ugp = ""; }
 	  elsif ($line =~ /\"v/i) { $listLump[$curList] = $vgp; $vgp = ""; }
 	  elsif ($line =~ /\"w/i) { $listLump[$curList] = $wgp; $wgp = ""; }
 	  elsif ($line =~ /\"y/i) { $listLump[$curList] = $ygp; $ygp = ""; }
-	  elsif ($line =~ /\"u/i) { $listLump[$curList] = $ugp; $ugp = ""; }
-	  elsif ($line =~ /\"t/i) { $listLump[$curList] = $tgp; $tgp = ""; }
-	  elsif ($line =~ /\"h/i) { $listLump[$curList] = $hgp; $hgp = ""; }
+	  elsif ($line =~ /\"s/i) { $listLump[$curList] = $sgp; $sgp = ""; } # stores
+	  elsif ($line =~ /\"h/i) { $listLump[$curList] = $hgp; $hgp = ""; } # H
+	  elsif ($line =~ /\"g/i) { $listLump[$curList] = $ggp; $ggp = ""; } # general
+	  elsif ($line =~ /\"o/i) { $listLump[$curList] = $ogp; $ogp = ""; } # opening
 	  $listLump[$curList] .= $line;
 	}
   }
@@ -135,17 +153,21 @@ if ($allCaps) { splice(@listLump, $rolling, 0, $allCaps); $rolling++; }
 if ($newTitles) { splice(@listLump, $rolling, 0, $newTitles); $rolling++; }
 if ($shoutyAds) { splice(@listLump, $rolling, 0, $shoutyAds); $rolling++; }
 if ($movies) { splice(@listLump, $rolling, 0, $movies); $rolling++; }
-if ($fgp) { splice(@listLump, $rolling, 0, $fgp); $rolling++; }
+if ($fgp) { splice(@listLump, $rolling, 0, $fgp); $rolling++; } # start SA areas
 if ($mgp) { splice(@listLump, $rolling, 0, $mgp); $rolling++; }
 if ($igp) { splice(@listLump, $rolling, 0, $igp); $rolling++; }
 if ($xgp) { splice(@listLump, $rolling, 0, $xgp); $rolling++; }
+if ($rgp) { splice(@listLump, $rolling, 0, $rgp); $rolling++; } # end SA areas
 if ($pgp) { splice(@listLump, $rolling, 0, $pgp); $rolling++; }
+if ($tgp) { splice(@listLump, $rolling, 0, $tgp); $rolling++; }
+if ($ugp) { splice(@listLump, $rolling, 0, $ugp); $rolling++; }
 if ($vgp) { splice(@listLump, $rolling, 0, $vgp); $rolling++; }
 if ($wgp) { splice(@listLump, $rolling, 0, $wgp); $rolling++; }
 if ($ygp) { splice(@listLump, $rolling, 0, $ygp); $rolling++; }
-if ($ugp) { splice(@listLump, $rolling, 0, $ugp); $rolling++; }
-if ($tgp) { splice(@listLump, $rolling, 0, $tgp); $rolling++; }
+if ($sgp) { splice(@listLump, $rolling, 0, $sgp); $rolling++; }
 if ($hgp) { splice(@listLump, $rolling, 0, $hgp); $rolling++; }
+if ($ggp) { splice(@listLump, $rolling, 0, $ggp); $rolling++; }
+if ($ogp) { splice(@listLump, $rolling, 0, $ogp); $rolling++; }
 
 my @finalOut;
 
