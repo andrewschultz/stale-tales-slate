314675abb4b8d5ec1f720b49a6cc1e7ef4b0700a
andrewschultz <schultz.andrew@sbcglobal.net>
2018-07-08 14:22:45 -0500

    hch now prints to file

--- utils/hch.py
+++ utils/hch.py
@@ -14,7 +14,13 @@ ignore_nudmis = False
 err_max = 0
 quiet = 0
 
+spec_detail = defaultdict(str)
+
 region_wildcard = ""
+
+out_to_file = False
+launch_outfile = False
+houtfile = "hch_out.txt"
 
 def standard_usage():
     print("hch.py 1 si e50 = print 50 maximum errors from shuffling (1) ignoring NUDMIS (nudge/miss) output file testing")
@@ -29,6 +35,16 @@ def usage():
     print("q = quiet")
     print("?? = standard usage")
     exit()
+
+def lastrev(x):
+    x2 = x.lower()
+    for y in range(-1, -len(x), -1):
+        if x2[y] != x2[-1]: return x[:y] + x[-1] + x[y:-1]
+    return x[::-1] # might as well reverse if the last n-1 letters are the same
+
+def noquo(x):
+    temp = re.sub("^\"", "", x)
+    return re.sub("\".*", "", temp)
 
 def no_of_for(x):
     return re.sub(".* (for|of) ", "", x.strip())
@@ -68,7 +84,9 @@ def find_in_glob(spec_stuff, pattern, b, region):
     for q in sorted(list(set(spec_stuff.keys()) | set(got_spec_yet.keys())), key = lambda x: spec_stuff[x] if x in spec_stuff.keys() else -1):
         if q not in got_spec_yet.keys():
             errs += 1
-            if err_max == 0 or errs <= err_max: print(pattern, ':', q, "in table of", b, "but not in", pattern, spec_stuff[q], jump_str(spec_stuff[q], last_line))
+            if err_max == 0 or errs <= err_max:
+                print(pattern, ':', q, "in table of", b, "but not in", pattern, spec_stuff[q], jump_str(spec_stuff[q], last_line))
+                if out_to_file: hout.write("#{:s} for {:s}\n>{:s}\n{:s}\n\n".format(b, q, lastrev(q), spec_detail[q]))
             last_line = spec_stuff[q]
         if q not in spec_stuff.keys():
             errs += 1
@@ -90,9 +108,11 @@ def spec_check(a, b, region=""):
             if not in_spechelp: continue
             if line.startswith("xtrhelp"): continue
             if not line.strip() or "\t" not in line: break
-            ary = line.strip().lower().split("\t")
+            ary = line.strip().split("\t")
+            ary[0] = ary[0].lower()
             if ary[0] in needs_spec_test.keys(): sys.exit("STORY.NI duplication ({:s}): {:s} already defined at line {:d}, redefined at line {:d}.".format(b, ary[0], needs_spec_test[ary[0]], line_count))
             needs_spec_test[ary[0]] = line_count
+            spec_detail[ary[0]] = noquo(ary[1])
             if debug: print(ary[0])
     rbr_find = "rbr-*"
     reg_find = "*-nudmis*"
@@ -110,6 +130,8 @@ while count < len(sys.argv):
     if arg == '1' or arg == 'sa': projs = ['sa']
     elif arg == '2' or arg == 'roi' or arg == 'ro' or arg == 'r': projs = ['roi']
     elif arg == 'q': quiet = True
+    elif arg == 'o': out_to_file = True
+    elif arg == 'lo' or arg == 'ol': out_to_file = launch_outfile = True
     elif arg == 'nq' or arg == 'qb': quiet = False
     elif re.search("^[asdi]+", arg):
         tabs = []
@@ -122,6 +144,13 @@ while count < len(sys.argv):
     else: usage()
     count += 1
 
+if out_to_file: hout = open(houtfile, "w")
+
 for q in projs:
     for t in tabs:
         spec_check(q, t, region_wildcard)
+
+if out_to_file:
+    print("Wrote to", houtfile)
+    hout.close()
+    if launch_outfile: i7.npo(houtfile)
