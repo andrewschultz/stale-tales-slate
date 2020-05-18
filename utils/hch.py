a5022c23b127b35414b8345c80db3d422000723e
andrewschultz <schultz.andrew@sbcglobal.net>
2020-05-17 21:39:07 -0500

    printing details + verifying output REG files in hch

--- utils/hch.py
+++ utils/hch.py
@@ -13,6 +13,7 @@ import sys
 
 debug = False
 ignore_nudmis = False
+print_details = False
 err_max = 0
 quiet = 0
 
@@ -43,6 +44,43 @@ def usage():
     print("?? = standard usage")
     exit()
 
+def my_category(my_file):
+    if 'nudmis' in my_file: return 'nudmis'
+    if 'slider' in my_file: return 'slider'
+    return ''
+
+ignore_blank_cat = True
+
+def verify_reg_files(my_proj):
+    print("This is hard coded. I need to fix it.")
+    x = glob.glob("reg-roi*.txt")
+    valids = { 'slider': [ 'slider' ],
+      'nudmis': [ 'scannotes', 'spechelp', 'done rejects' ],
+      '' : []
+    }
+    flaggable = defaultdict(bool)
+    for q in valids:
+        for r in valids[q]:
+            flaggable[r] = True
+    glo = 0
+    for fi in x:
+        loc = 0
+        cat = my_category(fi)
+        fb = os.path.basename(fi)
+        if ignore_blank_cat and not cat:
+            if not quiet: print("Skipping category for", fi)
+            continue
+        with open(fi) as file:
+            for (line_count, line) in enumerate(file, 1):
+                if not line.startswith('#'): continue
+                for y in flaggable:
+                    if line[1:].lower().startswith(y):
+                        if y not in valids[cat]:
+                            loc += 1
+                            glo += 1
+                            print("Invalid comment {}/{} {} line {}: {}".format(loc, glo, fb, line_count, line.strip()))
+    exit()
+
 def lastrev(x):
     x2 = x.lower()
     for y in range(-1, -len(x), -1):
@@ -61,7 +99,7 @@ def jump_str(a, b):
     if a - b <= 1: return ''
     return "*JUMP* {}".format(a-b-1)
 
-def find_in_glob(sync_stuff, pattern, b, region, extras = []):
+def find_in_glob(sync_stuff, pattern, b, region, details, extras = []):
     for_pattern = "for pattern {} in {}.".format(pattern, b)
     got_sync_yet = defaultdict(str)
     errs = 0
@@ -116,12 +154,20 @@ def find_in_glob(sync_stuff, pattern, b, region, extras = []):
             errs += 1
             if err_max == 0 or errs <= err_max:
                 print(pattern, ':', q, "in table of", b, sync_stuff[q], "but not in", pattern, jump_str(sync_stuff[q], last_line))
+                if print_details:
+                    print("#{} for {}".format(b, q))
+                    if b == 'scannotes':
+                        print(">scan {}".format(q))
+                    else:
+                        print(">{}".format(q))
+                    print(details[q])
                 if out_to_file: hout.write("#{:s} for {:s}\n>{:s}\n{:s}\n\n".format(b, q, 'read ' + q if b == 'readables' else lastrev(q), sync_detail[q]))
             elif err_max and errs == err_max + 1: print("Too many errors. Increase with -e##.")
             last_line = sync_stuff[q]
         if q not in sync_stuff.keys():
             errs += 1
-            if err_max == 0 or errs <= err_max: print(pattern, ':', q, "in", pattern, sync_stuff[q], "but not in table of", b, '*jump' if sync_stuff[q] - last_line > 1 and last_line else '')
+            if err_max == 0 or errs <= err_max:
+                print(pattern, ':', q, "in", pattern, sync_stuff[q], "but not in table of", b, '*jump' if sync_stuff[q] - last_line > 1 and last_line else '')
             last_line = sync_stuff[q]
     print (errs if errs else "No", "errors found", for_pattern)
     return errs
@@ -133,6 +179,7 @@ def sync_check(a, b, region=""):
     i7.go_proj(a)
     ever_syncable_table = False
     main_or_table = [ i7.main_src(a), i7.hdr(a, 'ta') ]
+    detail_column = cols[b] if b in cols else 1
     for fi in main_or_table:
         with open(fi) as file:
             for (line_count, line) in enumerate(file, 1):
@@ -140,6 +187,7 @@ def sync_check(a, b, region=""):
                     in_syncable_table = True
                     ever_syncable_table = True
                     reading_header = '(continued)' not in line
+                    print("Started", table_to_find, "at line", line_count, "file", os.path.basename(fi))
                     continue
                 if not in_syncable_table: continue
                 if reading_header:
@@ -150,21 +198,23 @@ def sync_check(a, b, region=""):
                 ary[0] = ary[0].lower()
                 if ary[0] in needs_sync_test.keys(): sys.exit("STORY.NI duplication ({:s}): {:s} already defined at line {:d}, redefined at line {:d}.".format(b, ary[0], needs_sync_test[ary[0]], line_count))
                 needs_sync_test[ary[0]] = line_count
-                sync_detail[ary[0]] = noquo(ary[1])
+                sync_detail[ary[0]] = noquo(ary[detail_column])
                 if debug: print(ary[0])
     if not ever_syncable_table:
         print(a, "/", b, "was not found in main source or table header.")
         return 0
     rbr_find = "rbr-*"
     reg_find = "*-nudmis*"
-    ret_val = find_in_glob(needs_sync_test, rbr_find, b, region, ["reg-roi-seed.txt"] if os.path.exists("reg-roi-seed.txt") else [])
-    if not ignore_nudmis: ret_val += find_in_glob(needs_sync_test, reg_find, b, region)
+    ret_val = find_in_glob(needs_sync_test, rbr_find, b, region, sync_detail, ["reg-roi-seed.txt"] if os.path.exists("reg-roi-seed.txt") else [])
+    if not ignore_nudmis: ret_val += find_in_glob(needs_sync_test, reg_find, b, region, sync_detail)
     return ret_val
 
 projs = ['sa', 'roi']
 tabs = { 'sa' : ['spechelp', 'done rejects', 'readables'],
-  'roi': ['aftertexts', 'spechelp', 'done rejects', 'readables']
+  'roi': ['scannotes', 'spechelp', 'done rejects', 'readables']
 }
+
+cols = { 'scannotes': 5 }
 
 count = 1
 
@@ -177,9 +227,12 @@ while count < len(sys.argv):
     elif arg == 'o': out_to_file = True
     elif arg == 'w': show_wrongs = True
     elif arg == 'nw' or arg == 'wn': show_wrongs = False
+    elif arg == 'pd' or arg == 'dp': print_details = True
+    elif arg == 'npd' or arg == 'ndp' or arg == 'pdn' or arg == 'dpn': print_details = False
     elif arg == 'lo' or arg == 'ol': out_to_file = launch_outfile = True
     elif arg == 'nq' or arg == 'qb': quiet = False
     elif arg == 'i': ignore_nudmis = True
+    elif arg == 'v': verify_reg_files('roi')
     elif re.search("^[asdir]+", arg):
         if 'a' in arg: tabs.append('aftertexts')
         if 's' in arg: tabs.append('spechelp')
