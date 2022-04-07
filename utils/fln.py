d1b119352d4f728b2759e1792bf7cb68263bdb37
Andrew <blurglecruncheon@gmail.com>
2022-04-07 04:10:22 -0500

    fln.py can now output red-text candidates

--- utils/fln.py
+++ utils/fln.py
@@ -2,8 +2,11 @@
 # so much quicker than PERL!
 # it takes a word, pulls a first name from it, then checks if the remaining string is in the "last words" dict
 
+import colorama
 import sys
 from collections import defaultdict
+
+red_check = True
 
 def show_exacts(my_name, my_dict, my_description = "no description"):
     matches = []
@@ -16,28 +19,44 @@ def show_exacts(my_name, my_dict, my_description = "no description"):
         print("No matches for {} and {}.".format(my_name, my_description))
 
 def read_names(file_string):
-	temp_dict = defaultdict(list)
-	with open(file_string) as file:
-		for (line_count, line) in enumerate(file, 1):
-			my_entry = line.lower().strip()
-			temp_dict[''.join(sorted(list(my_entry)))].append(my_entry)
-	return temp_dict
+    temp_dict = defaultdict(list)
+    with open(file_string) as file:
+        for (line_count, line) in enumerate(file, 1):
+            my_entry = line.lower().strip()
+            temp_dict[''.join(sorted(list(my_entry)))].append(my_entry)
+    return temp_dict
 
 def remove_from(my_name, my_list):
-	for n in my_name:
-		try:
-			my_list.remove(n)
-		except:
-			return ''
-	return ''.join(my_list)
+    for n in my_name:
+        try:
+            my_list.remove(n)
+        except:
+            return ''
+    return ''.join(my_list)
 
 first_names = read_names("c:/writing/dict/firsts.txt")
 last_names = read_names("c:/writing/dict/lasts.txt")
 
+def any_match(str1, str2):
+    if len(str1) != len(str2):
+        return False
+    for x in range(0, len(str1)):
+        if str1[x] == str2[x]:
+            return True
+    return False
+
+def check_reds(wanted_string, firsts, lasts):
+    for f in firsts:
+        for l in lasts:
+            match_string = f + l
+            if any_match(wanted_string, match_string):
+                continue
+            print(colorama.Fore.RED + "  Red name:", f, l, colorama.Style.RESET_ALL)
+
 try:
-	my_name = sys.argv[1]
+    my_name = sys.argv[1]
 except:
-	sys.exit("Need a name or word.")
+    sys.exit("Need a name or word.")
 
 smn = set(my_name)
 
@@ -47,10 +66,12 @@ show_exacts(my_name, last_names, "last names")
 print("Two-name matches:")
 
 for f in first_names:
-	stemp = sorted(list(my_name))
-	poss_last = remove_from(f, stemp)
-	if not poss_last:
-		continue
-	if poss_last not in last_names:
-		continue
-	print("got", '/'.join(first_names[f]), '/'.join(last_names[poss_last]))
+    stemp = sorted(list(my_name))
+    poss_last = remove_from(f, stemp)
+    if not poss_last:
+        continue
+    if poss_last not in last_names:
+        continue
+    print("got", '/'.join(first_names[f]), '/'.join(last_names[poss_last]))
+    if red_check:
+        check_reds(my_name, first_names[f], last_names[poss_last])
